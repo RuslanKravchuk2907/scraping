cf8cb240a9b09b28d7876c907a666d402d368a40
melad <meladzeruslan164@gmail.com>
2023-12-28 21:38:59 +0200

    28.12

---
+++ Exel.py
@@ -0,0 +1,28 @@
+import xlsxwriter
+from parsing import array
+
+
+def writer(parametr):
+    book = xlsxwriter.Workbook(r"C:\Users\melad\PycharmProjects\scraping\exel.xlsx")
+    page = book.add_worksheet("Merchandise")
+
+
+    row = 0
+    column = 0
+
+    page.set_column("A:A", 20)
+    page.set_column("B:B", 20)
+    page.set_column("C:C", 50)
+    page.set_column("D:D", 50)
+
+
+    for item in parametr():
+        page.write(row, column, item[0])
+        page.write(row, column+1, item[1])
+        page.write(row, column+2, item[2])
+        page.write(row, column+3, item[3])
+        row +=1
+
+    book.close()
+
+writer(array)
--- parsing.py
+++ parsing.py
@@ -2,41 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = []
 
 headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
-for count in range(1,8):
+def download(url):
+    resp = requests.get(url, stream=True)
+    r = open("C:\\Users\\melad\\PycharmProjects\\scraping\\image\\" + url.split("/")[-1], "wb")
+    for value in resp.iter_content(1024*1024):
+        r.write(value)
+    r.close()
 
-    sleep(3)
+def get_url():
+    for count in range(1,8):
 
-    url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+        url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml") # parser
+        data = soup.find_all("div", class_="w-full rounded border") #all information about a specific product
+        for i in data:
 
-    response = requests.get(url, headers=headers)
+            card_url = "https://scrapingclub.com" + i.find("a").get("href")
+            yield card_url
+def array():
+    for card_url in get_url():
 
-    soup = BeautifulSoup(response.text, "lxml") # parser
-
-    data = soup.find_all("div", class_="w-full rounded border") #all information about a specific product
-
-    for i in data:
-
-       card_url = "https://scrapingclub.com" + i.find("a").get("href")
-       list_card_url.append(card_url)
-
-for card_url in list_card_url:
-
-    response = requests.get(card_url, headers=headers)
-
-    soup = BeautifulSoup(response.text, "lxml") # parser
-
-    data = soup.find("div", class_="my-8 w-full rounded border")
-
-    name = data.find("h3", class_="card-title").text
-
-    price = data.find("h4").text
-
-    text = data.find("p", class_ = "card-description").text
-
-    url_img ="https://scrapingclub.com" + data.find("img", class_="card-img-top").get("src")
-
-    print(name + "\n" + price + "\n" + text + "\n" + url_img + "\n\n")
+        response = requests.get(card_url, headers=headers)
+        sleep(1)
+        soup = BeautifulSoup(response.text, "lxml") # parser
+        data = soup.find("div", class_="my-8 w-full rounded border")
+        name = data.find("h3", class_="card-title").text
+        price = data.find("h4").text
+        text = data.find("p", class_ = "card-description").text
+        url_img ="https://scrapingclub.com" + data.find("img", class_="card-img-top").get("src")
+        download(url_img)
+        yield name, price, text, url_img
